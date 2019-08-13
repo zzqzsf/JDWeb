505b2a234de55b6429708742cb34140339047b33
hhh123-sec <1011267132@qq.com>
2019-08-13 08:58:13 +0800

    aaaaa

---
+++ src/main/java/org/lanqiao/jdlogin/demo/entity/User.java
@@ -0,0 +1,107 @@
+package org.lanqiao.jdlogin.demo.entity;
+
+public class User {
+    private Integer userId;
+
+    private String userName;
+
+    private String password;
+
+    private String telephone;
+
+    private String sex;
+
+    private String email;
+
+    private String idcard;
+
+    private String userStatus;
+
+    public User() {
+    }
+
+    public User(String userName, String password, String telephone,String email) {
+        this.userName = userName;
+        this.password = password;
+        this.telephone = telephone;
+        this.email = email;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone == null ? null : telephone.trim();
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getIdcard() {
+        return idcard;
+    }
+
+    public void setIdcard(String idcard) {
+        this.idcard = idcard == null ? null : idcard.trim();
+    }
+
+    public String getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(String userStatus) {
+        this.userStatus = userStatus == null ? null : userStatus.trim();
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", sex='" + sex + '\'' +
+                ", email='" + email + '\'' +
+                ", idcard='" + idcard + '\'' +
+                ", userStatus='" + userStatus + '\'' +
+                '}';
+    }
+}
